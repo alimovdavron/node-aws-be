0909602a5f9152c8dde16212ff6cb4e606637810
Davron Alimov <Davron_Alimov@burberry.com>
2021-08-11 18:02:57 +0500

    WIP

--- import-service/src/libs/s3.ts
+++ import-service/src/libs/s3.ts
@@ -18,6 +18,37 @@ class S3 {
         }).promise();
     }
 
+    public copyObject = async (fileKey: string): Promise <any> => {
+        return new Promise ((resolve, reject) => {
+            this.s3.copyObject({
+                Bucket: this.bucketName,
+                CopySource: fileKey,
+                Key: fileKey.replace("uploaded", "parsed"),
+            }, (err, data) => {
+                if(err) {
+                    reject(err)
+                } else resolve(data);
+            })
+        })
+    }
+
+    public removeObject = async (fileKey: string): Promise <any> => {
+        return new Promise((resolve, reject) => {
+            this.s3.deleteObject({
+                Bucket: this.bucketName,
+                Key: fileKey
+            }, (err, data) => {,
+                if(err) {
+                    reject(err);
+                } else resolve(data);
+            })
+        })
+    }
+
+    public moveFileFromFolder = async (fileKey: string, source: string, destination) => {
+
+    }
+
     getSignedPutUrl = async (fileName: string, timeout: number = 120) => {
         return await this.s3.getSignedUrlPromise(
             'putObject',
