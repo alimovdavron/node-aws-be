13a05a17dc83661d2bc918b6db30b85324ae6634
Davron Alimov <Davron_Alimov@burberry.com>
2021-10-12 10:32:39 +0500

    fix

--- bff-service/src/main.ts
+++ bff-service/src/main.ts
@@ -2,13 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as path from 'path';
 import * as dotenv from 'dotenv';
-import {AllExceptionsFilter} from "./exception.filter";
 
 async function bootstrap() {
   await dotenv.config({
-    path: path.resolve(__dirname, '../.env.eb'),
+    path: path.resolve(__dirname, '../.env'),
   });
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
